925b0e28e9145ba5c4e6d9346e187d2b772a89e9
Valentyn Tulub <valentyntu@softwareplanet.uk.com>
2018-07-24 17:46:55 +0300

    Add: function to get all user's saves;

--- src/Workspace/Controls/Storage/Remote/CloudLoader/CloudLoader.js
+++ src/Workspace/Controls/Storage/Remote/CloudLoader/CloudLoader.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import "./CloudLoader.css"
+import savesURL from "../savesURL"
 
 class CloudLoader extends Component {
     render() {
@@ -13,7 +14,20 @@ class CloudLoader extends Component {
     }
 
     componentDidMount() {
+    }
 
+    loadCurrentUsersSaves() {
+        if (this.props.auth.isAuthenticated()) {
+            return this.props.auth.getUserInfo()
+                .then(info =>
+                    fetch(`${savesURL}?sub=${info.sub}`, {
+                            method: 'GET',
+                            headers: {
+                                'Content-Type': 'application/json'
+                            }
+                        }
+                    )).then(saves => saves.json());
+        } else return [];
     }
 }
 
